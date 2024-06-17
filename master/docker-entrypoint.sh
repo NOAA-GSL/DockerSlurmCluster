5e0ceee335cc6f6cce39360748886118bdbf955d
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2024-06-17 09:12:29 -0600

    Use official SchedMD debian RPMs to install Slurm (#8)
    * Use official SchedMD debian RPMs to install Slurm
    
    Update config files to accommodate newer version of Slurm.
    
    * Make the Dockefile architecture independent
    
    * Add step to build frontend on ARM
    
    * Add debug step to CI
    
    * Update to ubuntu 22.04
    
    * Fix inconsistent cores per socket
    
    * Remove duplicate clustername from slurm.conf
    
    * Remove unused env from CI workflow config
    
    * Use native arm runners for the arm builds and merge digests
    
    * Fix frontend digest names
    
    * Add shared munge key for slurm communication across containers
    
    Update image to ghcr.io.
    
    * Actually add the key, not just update the Dockerfiles
    
    * Allow up to 10 compute nodes
    
    * Remove testing volume mount on frontend
    
    * Remove leftover comment from CI workflow
    
    * Remove unnecessary action and update checkout action version

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export SLURM_CPUS_ON_NODE=$(cat /proc/cpuinfo | grep processor | wc -l)
-sudo sed -i "s/REPLACE_IT/CPUs=${SLURM_CPUS_ON_NODE}/g" /etc/slurm-llnl/slurm.conf
+sudo sed -i "s/REPLACE_IT/${SLURM_CPUS_ON_NODE}/g" /etc/slurm/slurm.conf
 
 sudo service munge start
-sudo service slurmctld start
+sudo slurmctld
 sudo service ssh start
 
 tail -f /dev/null
