c817f7ece1302a83851053d67a7d6091a581be29
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2026-05-20 19:12:56 -0600

    Fix permissions on .ssh directory

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Ensure SLURM_CPUS_ON_NODE is set
+export SLURM_CPUS_ON_NODE=${SLURM_CPUS_ON_NODE:-$(cat /proc/cpuinfo | grep processor | wc -l)}
 sudo sed -i "s/REPLACE_IT/${SLURM_CPUS_ON_NODE}/g" /etc/slurm/slurm.conf
 
 sudo service munge start
