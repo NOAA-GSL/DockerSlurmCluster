8f51993d37ee414e9ee459779aa66a1b1e0de509
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2026-06-10 15:48:56 -0600

    Add job accounting to enable retrieval of completed jobs (#23)
    * Add job accounting to enable retrieval of completed jobs
    
    * Add sacct test to amd builds
    
    * Add upgrade to OS install to get patches
    
    * Fix typo in master Dockerfile
    
    ---------
    
    Co-authored-by: christopherwharrop <christopher.w.harrop@gmail.com>

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -4,6 +4,25 @@ export SLURM_CPUS_ON_NODE=${SLURM_CPUS_ON_NODE:-$(cat /proc/cpuinfo | grep proce
 sudo sed -i "s/REPLACE_IT/${SLURM_CPUS_ON_NODE}/g" /etc/slurm/slurm.conf
 
 sudo -u munge /usr/sbin/munged
+
+sudo service mariadb start
+until sudo mariadb -e "SELECT 1" >/dev/null 2>&1; do
+	sleep 1
+done
+sudo mariadb <<-SQL
+	DROP USER IF EXISTS 'slurm'@'localhost';
+	CREATE USER 'slurm'@'localhost' IDENTIFIED VIA unix_socket;
+	CREATE DATABASE IF NOT EXISTS slurm_acct_db;
+	GRANT ALL ON slurm_acct_db.* TO 'slurm'@'localhost';
+	FLUSH PRIVILEGES;
+SQL
+
+sudo -u slurm slurmdbd
+until sudo -u slurm sacctmgr -i show cluster >/dev/null 2>&1; do
+	sleep 1
+done
+sudo -u slurm sacctmgr -i add cluster cluster >/dev/null 2>&1 || true
+
 sudo slurmctld
 if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
 	sudo ssh-keygen -A
